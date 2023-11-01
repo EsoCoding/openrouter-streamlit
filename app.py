56ef0a43d350146615eb45d36697d8b128f90b7f
EsoCode <arjanpoortman@gmail.com>
2023-11-01 19:31:33 +0100

    feat: Add new feature

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
 import os
-import AstroChat from
-from AstroChat import OpenRouterStreamChat
 
 
 def main():
-    OpenRouterStreamChat.start()
 
 
-# stream lit app
+    # stream lit app
 if __name__ == "__main__":
     main()
