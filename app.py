0f4a38390ef37771f1282405937b00b3fd50182d
EsoCode <arjanpoortman@gmail.com>
2023-11-01 19:26:32 +0100

    feat: delete 2 files and update 1 file Add new feature

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import os
-import AstroChat 
+import AstroChat from
+from AstroChat import OpenRouterStreamChat
 
-# launches main app
+
 def main():
-    AstroChat.start()
+    OpenRouterStreamChat.start()
+
 
 # stream lit app
 if __name__ == "__main__":
-    os.chdir(os.path.dirname(os.path.abspath(__file__)))
-    os.system("streamlit run app.py")
+    main()
